6445eb028caf4f5e68af900f6b4b1ef66b2e34c1
tonixhaker <tonixhaker@gmail.com>
2018-10-12 16:54:18 +0300

    Register init

---
+++ src/components/pages/auth/Registration.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import {Button} from 'antd';
+
+
+class Registration extends Component {
+
+    render() {
+        return(
+            <Button>sfsdfsd</Button>
+        );
+    }
+}
+
+export default Registration;
