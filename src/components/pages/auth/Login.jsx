770382504b774b686c1366c54afd40ba09af9551
Anton Danilov <tonixhaker@gmail.com>
2018-10-14 18:22:40 +0300

    Login form

--- src/components/pages/auth/Login.jsx
+++ src/components/pages/auth/Login.jsx
@@ -1,11 +1,26 @@
 import React, {Component, Fragment} from 'react';
+import LoginForm from '../../forms/LoginForm';
+import {Parallax} from 'react-parallax';
 
 class Login extends Component {
 
     render() {
         return(
             <Fragment>
-                <h1>Login</h1>
+                <div className={'login-block'}>
+                    <Parallax
+                        blur={5}
+                        bgImage={require('../../../img/login-back.jpg')}
+                        bgImageAlt="kivi"
+                        strength={200}
+                    >
+                        <div className={'flex flex-center paralaxcontent'}>
+                            <div className={'container'}>
+                                <LoginForm />
+                            </div>
+                        </div>
+                    </Parallax>
+                </div>
             </Fragment>
         );
     }
